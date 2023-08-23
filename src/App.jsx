f6996632df0aa4fa404fe1f6fcfa890f651dfb0d
Jamesxtmp <jamesxtmp@gmail.com>
2023-08-22 20:20:47 -0600

    scoll mobil solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TimeLine from "./components/timeline/TimeLine";
 
 export default function App () {
   return (
-    <div className="px-5 lg:flex lg:flex-col lg:items-center lg:w-full">
+    <div className="px-5 flex flex-col lg:items-center w-full">
       <Head />
       <div className="lg:max-w-[70%]" >
         <TimeLine />
