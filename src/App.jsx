bc0a2b55af890a23f00cd303b9a663687ed19c64
Jamesxtmp <jamesxtmp@gmail.com>
2023-09-15 19:19:39 -0600

    button close in gallery

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Head from "./components/head/Head";
 import TimeLine from "./components/timeline/TimeLine";
 
+import { RiCloseLine } from "react-icons/ri"
+
 import ImageFullPrev from "./components/emergents/ImageFullPrev"
 
 export default function App () {
@@ -14,14 +16,24 @@ export default function App () {
       <div className="lg:max-w-[70%]" >
         <TimeLine setterImages={setPrevImagesIsShow} />
       </div>
-      {prevImagesIsShow.length > 0 &&
+
+      {
+        // Muestra imagen si las hay
+        prevImagesIsShow.length > 0 &&
         <div>
-          <div
+          <div //Cobertura
             className="fixed top-0 right-0 w-full h-[100vh] bg-gray-900 bg-opacity-90"
             onClick={() => setPrevImagesIsShow( [] )}
           ></div>
-          <div className='p-4 fixed top-0 left-0'>
+          <div // Imagen
+            className='p-4 fixed top-0 left-0'>
             <ImageFullPrev imagesToView={prevImagesIsShow} />
+          </div>
+          <div //boton cierre
+            className="bg-gray-300 opacity-50 w-10 h-10 rounded-full fixed top-5 right-5 flex text-black text-2xl justify-center items-center text-center "
+            onClick={() => setPrevImagesIsShow( [] )}
+          >
+            <RiCloseLine />
           </div>
         </div>
       }
