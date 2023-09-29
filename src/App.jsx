9747c1e7c2719f2cba143ebc923dfc970e1fb517
Jamesxtmp <jamesxtmp@gmail.com>
2023-09-28 21:06:35 -0600

    inplement zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+// import { Outlet } from "react-router-dom";
+
 import Head from "./components/head/Head";
 import TimeLine from "./components/timeline/TimeLine";
+import ViewCorousel from "./components/emergents/ViewCorousel";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
-import { RiCloseLine } from "react-icons/ri"
-
-import ImageFullPrev from "./components/emergents/ImageFullPrev"
 
 export default function App () {
   const [prevImagesIsShow, setPrevImagesIsShow] = useState( [] )
@@ -12,30 +13,24 @@ export default function App () {
 
   return (
     <div className="px-5 flex flex-col lg:items-center w-full">
+
+
       <Head />
       <div className="lg:max-w-[70%]" >
         <TimeLine setterImages={setPrevImagesIsShow} />
       </div>
+      <Outlet
+        prevImagesIsShow={prevImagesIsShow}
+        setPrevImagesIsShow={setPrevImagesIsShow}
+      />
 
       {
         // Muestra imagen si las hay
         prevImagesIsShow.length > 0 &&
-        <div>
-          <div //Cobertura
-            className="fixed top-0 right-0 w-full h-[100vh] bg-gray-900 bg-opacity-90"
-            onClick={() => setPrevImagesIsShow( [] )}
-          ></div>
-          <div // Imagen
-            className='p-4 fixed top-0 left-0'>
-            <ImageFullPrev imagesToView={prevImagesIsShow} />
-          </div>
-          <div //boton cierre
-            className="bg-gray-300 opacity-50 w-10 h-10 rounded-full fixed top-5 right-5 flex text-black text-2xl justify-center items-center text-center "
-            onClick={() => setPrevImagesIsShow( [] )}
-          >
-            <RiCloseLine />
-          </div>
-        </div>
+        <ViewCorousel
+          prevImagesIsShow={prevImagesIsShow}
+          setPrevImagesIsShow={setPrevImagesIsShow}
+        />
       }
     </div>
   )
---
+++ src/routes/route.jsx
@@ -0,0 +1,15 @@
+import App from "../App"
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter( [
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "image/:id",
+        element: <div>Image</div>,
+      }
+    ]
+  },
+] );
